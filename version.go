64d3c40bc1d9db8a89743ca16190e365fa0b5339
y_uuki <yuki.tsubo@gmail.com>
2017-10-16 23:38:32 +0900

    Bump version 0.3.0

--- version.go
+++ version.go
@@ -4,7 +4,7 @@ package main
 const Name = "binrep"
 
 // Version is application version
-const Version string = "0.2.0"
+const Version string = "0.3.0"
 
 // GitCommit describes latest commit hash.
 // This is automatically extracted by git describe --always.
